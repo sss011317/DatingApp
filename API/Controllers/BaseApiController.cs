e7dea8fc179985e5b57ab480a9e25994255a9dca
sss011317 <sss011317@gmail.com>
2021-01-07 17:19:36 +0800

    add:HMACSHA512

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+        public BaseApiController() 
+        {
+        }
+
+        
+    }
+}
