e842c3f3843cb87c1eecebbd19c3f383ff85580c
lin <sss011317@yahoo.com.tw>
2021-01-10 16:50:06 +0800

    add:extionsFolder tide up ServiceConfigurations

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,16 +13,20 @@ namespace API.Controllers
     public class AccountController : BaseApiController
     {
         private readonly DataContext _context;
-        public AccountController(DataContext context)
+        private readonly ITokenService _tokenService;
+        public AccountController(DataContext context, ITokenService tokenService)
         {
+            _tokenService = tokenService;
             _context = context;
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register([FromBody]RegisterDto registerDto){
-            if(await UserExists(registerDto.Username)) return BadRequest("Username is Taken");
+        public async Task<ActionResult<UserDto>> Register([FromBody] RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is Taken");
             using var hmac = new HMACSHA512();
-            var user = new AppUser{
+            var user = new AppUser
+            {
                 UserName = registerDto.Username.ToLower(),
                 PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
@@ -30,24 +35,35 @@ namespace API.Controllers
             _context.Add(user);
             await _context.SaveChangesAsync();
 
-            return user;
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreatToken(user)
+            };
         }
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login([FromBody]LoginDto loginDto){
-            var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName==loginDto.Username);
-            if(user == null) return Unauthorized("Invalid username");
+        public async Task<ActionResult<UserDto>> Login([FromBody] LoginDto loginDto)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username);
+            if (user == null) return Unauthorized("Invalid username");
 
             using var hamc = new HMACSHA512(user.PasswordSalt);
             var computedHash = hamc.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
-            for(int i =0;i < computedHash.Length ; i++){
-                if(computedHash[i] !=user.PasswordHash[i]) return Unauthorized("Invalid Password");
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
             }
-            return user;
-        } 
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreatToken(user)
+            };
+        }
 
-        private async Task<bool> UserExists(string username){
-           return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,7 +17,7 @@ namespace API.Controllers
         {
             _context = context;
         }
-
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
@@ -25,6 +26,7 @@ namespace API.Controllers
          
         }
         // api/users/{id} 
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUsers(int id)
         {
